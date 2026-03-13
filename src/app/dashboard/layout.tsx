6e8ab1109a51a09e46b0d9080a63314bd0331599
Miguel Sánchez <m.sanchez.design.ux.ui@gmail.com>
2026-03-13 12:22:11 -0300

    fix layout dashboar
    fix layout dashboar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,36 +1,10 @@
-import localFont from "next/font/local";
-import "../../app/globals.css";
-import FlowbiteInit from '@/components/FlowbiteInit';
 import Header from '@/components/Header';
 import Sidebar from "@/components/sidebar/Sidebar";
-
-// Configuración de Montserrat Local con toda la familia de fuentes
-const montserrat = localFont({
-  src: [
-    { path: "../../fonts/Montserrat-Thin.ttf", weight: "100", style: "normal" },
-    { path: "../../fonts/Montserrat-ThinItalic.ttf", weight: "100", style: "italic" },
-    { path: "../../fonts/Montserrat-Light.ttf", weight: "300", style: "normal" },
-    { path: "../../fonts/Montserrat-LightItalic.ttf", weight: "300", style: "italic" },
-    { path: "../../fonts/Montserrat-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../../fonts/Montserrat-Italic.ttf", weight: "400", style: "italic" },
-    { path: "../../fonts/Montserrat-Medium.ttf", weight: "500", style: "normal" },
-    { path: "../../fonts/Montserrat-MediumItalic.ttf", weight: "500", style: "italic" },
-    { path: "../../fonts/Montserrat-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "../../fonts/Montserrat-SemiBoldItalic.ttf", weight: "600", style: "italic" },
-    { path: "../../fonts/Montserrat-Bold.ttf", weight: "700", style: "normal" },
-    { path: "../../fonts/Montserrat-BoldItalic.ttf", weight: "700", style: "italic" },
-    { path: "../../fonts/Montserrat-ExtraBold.ttf", weight: "800", style: "normal" },
-    { path: "../../fonts/Montserrat-Black.ttf", weight: "900", style: "normal" },
-  ],
-  variable: "--font-montserrat",
-  display: 'swap',
-});
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <FlowbiteInit />
-      <div className={`${montserrat.variable} antialiased flex h-screen w-full bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100`}>
+      <div className="flex h-screen w-full bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100">
         {/* Backoffice Menu */}
         <Sidebar />
 
