be8309bec36d4c0c08d2d385e5d1333772481ba1
Miguel Sánchez <m.sanchez.design.ux.ui@gmail.com>
2026-03-16 15:07:26 -0300

    Fix config main font
    Fix config main font Montserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import FlowbiteInit from '@/components/FlowbiteInit';
-
-// Configuración de Montserrat Local con toda la familia de fuentes
-const montserrat = localFont({
-  src: [
-    { path: "../fonts/Montserrat-Thin.ttf", weight: "100", style: "normal" },
-    { path: "../fonts/Montserrat-ThinItalic.ttf", weight: "100", style: "italic" },
-    { path: "../fonts/Montserrat-Light.ttf", weight: "300", style: "normal" },
-    { path: "../fonts/Montserrat-LightItalic.ttf", weight: "300", style: "italic" },
-    { path: "../fonts/Montserrat-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../fonts/Montserrat-Italic.ttf", weight: "400", style: "italic" },
-    { path: "../fonts/Montserrat-Medium.ttf", weight: "500", style: "normal" },
-    { path: "../fonts/Montserrat-MediumItalic.ttf", weight: "500", style: "italic" },
-    { path: "../fonts/Montserrat-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "../fonts/Montserrat-SemiBoldItalic.ttf", weight: "600", style: "italic" },
-    { path: "../fonts/Montserrat-Bold.ttf", weight: "700", style: "normal" },
-    { path: "../fonts/Montserrat-BoldItalic.ttf", weight: "700", style: "italic" },
-    { path: "../fonts/Montserrat-ExtraBold.ttf", weight: "800", style: "normal" },
-    { path: "../fonts/Montserrat-Black.ttf", weight: "900", style: "normal" },
-  ],
-  variable: "--font-montserrat",
-  display: 'swap',
-});
 
 export const metadata: Metadata = {
   title: "Origin Solutions - Backoffice Starter",
@@ -37,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={`${montserrat.variable} antialiased`}>
+      <body className="font-sans antialiased">
         {/* Inicializador de Flowbite para componentes interactivos */}
         <FlowbiteInit />
         {children}
